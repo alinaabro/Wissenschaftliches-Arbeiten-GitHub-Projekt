78b376599579366eddb679ae2fb6f430e3f7c2d5
Hasan222222 <154332293+Hasan222222@users.noreply.github.com>
2024-02-19 14:58:06 +0100

    Update Wissenschaftliches Arbeiten.R
    Hab die Aufgabe 2a) hochgeladen

--- Wissenschaftliches Arbeiten.R
+++ Wissenschaftliches Arbeiten.R
@@ -58,3 +58,37 @@ daten$Name <- NULL
 daten$Ticket <- NULL
 daten$Cabin <- NULL
 
+
+#2)a)
+deskriptive_stat <- function(daten) {
+   
+   # berechnet arithmetisches Mittel 
+  mean_value <- mean(daten, na.rm=T)
+  
+  # berechnet Median
+  median_value <- median(daten, na.rm=T)
+  
+  # berechnet Standartabweichung 
+  sd_value <- sd(daten, na.rm=T)
+  
+  # berechnet Varianz
+  var_value <- var(daten, na.rm=T)
+  
+  # berechnet Minimum und Maximum
+  min_value <- min(daten, na.rm=T)
+  max_value <- max(daten, na.rm=T)
+  
+  # berechnet Quantile
+  quartiles <- quantile(daten, probs = c(0.25, 0.75), na.rm=T)
+  
+  # Gibt die Ergebnisse raus 
+  cat("Mean:", mean_value, "\n")
+  cat("Median:", median_value, "\n")
+  cat("Standard Deviation:", sd_value, "\n")
+  cat("Variance:", var_value, "\n")
+  cat("Minimum:", min_value, "\n")
+  cat("Maximum:", max_value, "\n")
+  cat("1st Quartile:", quartiles[1], "\n")
+  cat("3rd Quartile:", quartiles[2], "\n")
+}
+
