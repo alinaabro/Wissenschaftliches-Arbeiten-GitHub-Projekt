b8ad49df310f708e069b6ab4af2466200772d631
AkMun47 <154274074+AkMun47@users.noreply.github.com>
2024-02-19 19:35:09 +0100

    Update Wissenschaftliches Arbeiten.R
    Hier der Code für die Aufgabe 2d). Habt ihr noch Vorschläge für andere Korrelationsmaße?

--- Wissenschaftliches Arbeiten.R
+++ Wissenschaftliches Arbeiten.R
@@ -92,3 +92,23 @@ deskriptive_stat <- function(daten) {
   cat("3rd Quartile:", quartiles[2], "\n")
 }
 
+#2iv)
+#Maße. fuer den Zusammenhang einer binären und einer 
+#metrisch skalierten Variable
+katbinom <- function(x,y, na.rm = FALSE){
+  x <- as.numeric(x)
+  y <- as.numeric(y)
+  if(na.rm == TRUE){
+    Tabelle <- data.frame(Pearson = cor(x,y,method = "pearson", use = "complete.obs"),
+                          Kendall =  cor(x,y,method = "kendall", use = "complete.obs"),
+                          Spearman = cor(x,y,method = "spearman", use = "complete.obs"))
+    print(Tabelle)
+  }
+  else{
+    Tabelle <- data.frame(Pearson = cor(x,y,method = "pearson", use = "all.obs"),
+                          Kendall =  cor(x,y,method = "kendall", use = "all.obs"),
+                          Spearman = cor(x,y,method = "spearman", use = "all.obs"))
+    print(Tabelle)
+  }
+}
+
