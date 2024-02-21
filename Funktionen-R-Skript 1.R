180fd916a54dcf1baceed656bfdd34da027a4b6b
Gleb2037 <160510315+Gleb2037@users.noreply.github.com>
2024-02-21 21:55:09 +0100

    Update Funktionen-R-Skript 1.R
    Punkt 2.vi geschrieben

--- Funktionen-R-Skript 1.R
+++ Funktionen-R-Skript 1.R
@@ -86,3 +86,57 @@ bivariate_stat <- function(variable1, variable2) {
   print(chiquadrat_test)
 }
 
+# 2.vi
+
+# Die Funktion plot_grouped_histogram erstellt ein Histogramm der Variable metrVar, das nach der Variable katVar gruppiert ist.
+# Mit legend_names kann man die Legendenbeschriftungen setzen
+# Zusätzliche Argumente können verwendet werden, um das grafische Layout anzupassen.
+# Beispiel: plot_grouped_histogram("Age", "Sex", legend_names = c("Maennlich", "Weiblich"), main = "Alterverteilung nach Geschlecht")
+
+plot_grouped_histogram <- function(metrVar, katVar, legend_names = NA, data = daten, ...) {
+  # Konvertiere katVar in einen Faktor, falls es noch keiner ist
+  data[[katVar]] <- factor(data[[katVar]])
+  
+  # Prüfe, ob katVar ein geordneter Faktor ist
+  if(is.ordered(data[[katVar]])) {
+    # Erhalte die eindeutigen Werte von katVar in der Reihenfolge der Faktorstufen
+    categories <- levels(data[[katVar]])
+  } else {
+    # Erhalte die eindeutigen Werte von katVar
+    categories <- unique(data[[katVar]])
+  }
+  
+  # Setze Farben mit Transparenz für die Kategorien
+  colors <- adjustcolor(rainbow(length(categories)), alpha = 0.5)
+  
+  # Bestimme die Intervallgrenzen für die Histogramme
+  hist_data <- hist(data[[metrVar]], plot = FALSE)
+  breaks <- hist_data$breaks
+  
+  # Erstelle eine Matrix, um die Histogrammzählungen für jede Kategorie zu speichern
+  hist_counts <- matrix(NA, nrow = length(categories), ncol = length(breaks) - 1)
+  
+  # Schleife durch jede Kategorie und berechne die Histogrammzählungen
+  for (i in 1:length(categories)) {
+    # Check for NA category
+    if (is.na(categories[i])) {
+      # Subset data with NA values in katVar
+      hist_counts[i, ] <- hist(data[is.na(data[[katVar]]),][[metrVar]], breaks = breaks, plot = FALSE)$counts
+      categories[i] <- "Keine Daten"
+    } else {
+      # Subset data for current category
+      hist_counts[i, ] <- hist(data[data[[katVar]] == categories[i],][[metrVar]], breaks = breaks, plot = FALSE)$counts
+    }
+  }
+  
+  # Plotte die Histogramme
+  barplot(hist_counts, col = colors, beside = TRUE, names.arg = paste("(", round(breaks[-length(breaks)], 2), ",", round(breaks[-1], 2), "]", sep = ""), xlab = metrVar, ...)
+  
+  # Füge eine Legende hinzu
+  if (sum(is.na(categories)) > 0) {
+    categories <- as.vector(categories)
+    categories[is.na(categories)] <- "Keine Daten"
+  }
+  if (length(legend_names) > 1) legend("topright", legend = legend_names, fill = colors)
+  else legend("topright", legend = categories, fill = colors)
+}
