4f495d807abd844645e2ed3a9a2874b00bb9f94c
AkMun47 <154274074+AkMun47@users.noreply.github.com>
2024-02-25 20:36:02 +0100

    Update Funktionen-R-Skript 1.R

--- Funktionen-R-Skript 1.R
+++ Funktionen-R-Skript 1.R
@@ -1,4 +1,4 @@
-#2)a)
+#2(i)
 deskriptive_stat <- function(daten) {
   
   # berechnet arithmetisches Mittel 
@@ -59,13 +59,7 @@ bivariate_stat <- function(variable1, variable2) {
   print(chiquadrat_test)
 }
 
-#2(v) Erstellt einen Mosaikplot der gegebenen 3 Merkmale
-#visualisierung(merkmal1, merkmal2, merkmal3, "NameMerkmal1", "NameMerkmal2", "NameMerkmal3")
-visualisierung <- function(merkmal1, merkmal2, merkmal3, m1name=NA, m2name=NA, m3name=NA){
-   mosaicplot(table(merkmal1,merkmal2,merkmal3), main=m3name, xlab=m1name, ylab=m2name)
-}
-
-#2iv)
+#2(iv)
 #Maße fuer den Zusammenhang einer binären und einer 
 #metrisch skalierten Variable
 katbinom <- function(x,y, na.rm = FALSE){
@@ -85,7 +79,13 @@ katbinom <- function(x,y, na.rm = FALSE){
   }
 }
 
-#2.vi
+#2(v) Erstellt einen Mosaikplot der gegebenen 3 Merkmale
+#visualisierung(merkmal1, merkmal2, merkmal3, "NameMerkmal1", "NameMerkmal2", "NameMerkmal3")
+visualisierung <- function(merkmal1, merkmal2, merkmal3, m1name=NA, m2name=NA, m3name=NA){
+   mosaicplot(table(merkmal1,merkmal2,merkmal3), main=m3name, xlab=m1name, ylab=m2name)
+}
+
+#2(vi)
 
 # Die Funktion plot_grouped_histogram erstellt ein Histogramm der Variable metrVar, das nach der Variable katVar gruppiert ist.
 # Mit legend_names kann man die Legendenbeschriftungen setzen
